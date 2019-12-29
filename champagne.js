adcb714797049455ca24434983ff91c58014b74c
yertto <yerrto@gmail.com>
2019-12-29 15:59:29 +1100

    Use relative radius steps instead of absolute radii.

--- champagne.js
+++ champagne.js
@@ -1,37 +1,49 @@
 var makerjs = require('makerjs');
 
 var Champagne = (function () {
-  function Champagne(height, width, border, pitch, radii0, radii1, radii2, radii3) {
+  function Champagne(height, width, border, maxRadius, step1, step2, step3, step4) {
     this.units = makerjs.unitType.Millimeter;
 
-    var radii = [radii0, radii1, radii2, radii3];
+    var radii = generateRadii(maxRadius, [step1, step2, step3, step4]);
 
-    this.paths = generateHoles(this, height, width, border, pitch, radii);
+    this.paths = generateHoles(this, height, width, border, maxRadius, radii);
     this.models = generateBorder(this, height, width, border);
   }
 
-  function generateHoles(_this, height, width, border, pitch, radii) {
-    var countX = Math.floor((height - (2 * border)) / pitch);
-    var countY = Math.floor((width  - (2 * border)) / pitch);
+  function generateHoles(_this, height, width, border, maxRadius, radii) {
+    var countX = Math.floor((height - (2 * border)) / (2 * maxRadius));
+    var countY = Math.floor((width  - (2 * border)) / (2 * maxRadius));
 
-    var holeRadii = shuffle(generateHoleRadii(height, width, countX, countY, radii));
+    var holeRadii = shuffle(generateHoleRadii(_this, height, width, countX, countY, radii));
 
     var holes = {};
     for (var ix = 0; ix < countY; ix++) {
       for (var iy = 0; iy < countX; iy++) {
         var radius = radii[radii.length - 1];
         var ir = ix + (iy * countY);
-        if (ir < (holeRadii.length - 1)) {
-          radius = holeRadii[ir];
-        }
+        if (ir < (holeRadii.length - 1)) radius = holeRadii[ir];
         holes[id(ix, iy)] = new makerjs.paths.Circle(
-          coords(border, ix, iy, pitch, jitter(radii[0] - radius)),
+          coords(border, ix, iy, maxRadius, jitter(radii[0] - radius)),
           radius
         );
       }
     }
 
     return holes;
+  }
+
+  function generateRadii(maxRadius, steps) {
+    var result = [];
+
+    var step = 0;
+    for (var is = 0; is < steps.length; is++) {
+      step += steps[is];
+      var radius = maxRadius - step;
+      if (radius > 0) result.push(radius);
+    }
+    if (result.length === 0) result = [maxRadius];
+
+    return result;
   }
 
   function id(ix, iy) {
@@ -42,14 +54,25 @@ var Champagne = (function () {
     return Math.floor((Math.random() * max * 2) - max);
   }
 
-  function coords(border, ix, iy, pitch, jitter) {
+  function coords(border, ix, iy, maxRadius, jitter) {
     return [
-      border + pitch/2 + ix * pitch + jitter,
-      border + pitch/2 + iy * pitch + jitter
+      border + maxRadius + ix * 2 * maxRadius + jitter,
+      border + maxRadius + iy * 2 * maxRadius + jitter
     ];
   }
 
-  function generateHoleRadii(height, width, countX, countY, radii) {
+  function displayNotes(_this, height, width, holesArea, radii) {
+    var totalArea = height * width;
+
+    _this.notes = "&nbsp;|&nbsp;" + '\n' +
+      "---- | ---" + '\n' +
+      "**Hole radii**: | " + radii + '\n' +
+      "**Holes Area**: |" + Math.round(holesArea) + "mm&#xb2;" + '\n' +
+      "**Total Area**: |" + totalArea + "mm&#xb2;" + '\n' +
+      "**Open Area**: |" + Math.round((100 * holesArea) / totalArea) + "%";
+  }
+
+  function generateHoleRadii(_this, height, width, countX, countY, radii) {
     var holeSizes = [];
     var holesArea = 0.0;
     var holeCount = countX * countY;
@@ -60,10 +83,7 @@ var Champagne = (function () {
       holesArea += Math.PI * radius * radius;
     }
 
-    var totalArea = height * width;
-    console.log("Holes Area: " + Math.round(holesArea) + "mm^2");
-    console.log("Total Area: " + totalArea + "mm^2");
-    console.log("Open Area: " + Math.round((100 * holesArea) / totalArea) + "%");
+    displayNotes(_this, height, width, holesArea, radii);
 
     return holeSizes;
   }
@@ -96,14 +116,14 @@ var Champagne = (function () {
 })();
 
 Champagne.metaParameters = [
-  { title: "Height (mm)"                  , type: "range", min: 10, max: 2000, step: 10, value: 110 },
-  { title: "Width (mm)"                   , type: "range", min: 20, max: 2000, step: 10, value: 210 },
-  { title: "Border (mm)"                  , type: "range", min:  2, max:   20, step:  1, value:   5 },
-  { title: "Distance between centres (mm)", type: "range", min: 20, max:  100, step:  1, value:  20 },
-  { title: "Circle A size (mm)"           , type: "range", min:  5, max:   50, step:  1, value:  10 },
-  { title: "Circle B size (mm)"           , type: "range", min:  5, max:   50, step:  1, value:   8 },
-  { title: "Circle C size (mm)"           , type: "range", min:  5, max:   20, step:  1, value:   6 },
-  { title: "Circle D size (mm)"           , type: "range", min:  5, max:   10, step:  1, value:   5 }
+  { title: "Height (mm)"         , type: "range", min: 10, max: 2000, step: 10, value: 110 },
+  { title: "Width (mm)"          , type: "range", min: 20, max: 2000, step: 10, value: 210 },
+  { title: "Border (mm)"         , type: "range", min:  2, max:   20, step:  1, value:   5 },
+  { title: "Max hole radius (mm)", type: "range", min: 10, max:  100, step:  1, value:  20 },
+  { title: "Step 1 (mm)"         , type: "range", min:  0, max:   20, step:  1, value:   4 },
+  { title: "Step 2 (mm)"         , type: "range", min:  0, max:   20, step:  1, value:   8 },
+  { title: "Step 3 (mm)"         , type: "range", min:  0, max:   20, step:  1, value:   4 },
+  { title: "Step 4 (mm)"         , type: "range", min:  0, max:   20, step:  1, value:   1 }
 ];
 
 module.exports = Champagne;
